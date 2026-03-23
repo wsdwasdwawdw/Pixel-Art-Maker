ea2e0ad65d061d75d8299b561da47252f9b3a05e
wsdwasdwawdw <jasonkarlrosales@gmail.com>
2026-03-23 22:40:58 +0800

    cells are now tables

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-let hasChild = false;
-function create(gridSize, canvas){
-    for(let i = 0; i < gridSize; i++){
-        const box = document.createElement("div");
-        box.classList.add("box");
-        canvas.appendChild(box);
+const table = document.querySelector("table");
+const color = document.querySelector(".color");
+const slider = document.querySelector(".slider");
+let currentColor = "#000000";
+
+function create(row, col){
+    
+    for(let i = 1; i <= row; i++){
+        const row = document.createElement("tr");
+        for(let j = 1; j <= col; j++){
+            const col = document.createElement("td");
+            col.className = "box";
+            row.appendChild(col);
+        }
+        table.appendChild(row);
     }
 
+    hasChild = true;
+    events();
+}
+
+function btn(){
+    table.innerHTML = "";
+    const rowInput = document.querySelector(".r");
+    const colInput = document.querySelector(".c");
+
+    const row = rowInput.value;
+    const col = colInput.value;
+
+    create(row, col);
+
+    rowInput.value = ""; 
+    colInput.value = "";
+    slider.value = 20;
+}
+
+function events(){
+    
+    color.addEventListener("change", ()=>{
+        currentColor = color.value;
+    });
+
+    slider.addEventListener("input", ()=>{
+        const boxes = document.querySelectorAll(".box");
+        boxes.forEach((box)=>{
+            box.style.height = `${slider.value}px`;
+            box.style.width = `${slider.value}px`;
+        })
+    });
+
+    
     const boxes = document.querySelectorAll(".box");
     boxes.forEach(element => {
         element.addEventListener("click", ()=>{
@@ -19,47 +62,6 @@ function create(gridSize, canvas){
             element.style.borderColor = "#000000";
         })
     });
-
-    hasChild = true;
 }
 
-function btn(){
-    const canvas = document.querySelector(".canvas");
-    const rowInput = document.querySelector(".r");
-    const colInput = document.querySelector(".c");
-
-    const row = rowInput.value;
-    const col = colInput.value;
-
-    const gridSize = row * col;
-    
-    canvas.style.gridTemplateColumns = `repeat(${col}, auto)`;
-    
-    if(hasChild){
-        const boxes = document.querySelectorAll(".box");
-        boxes.forEach((box)=>{
-            canvas.removeChild(box);
-        })
-    }
-
-    create(gridSize, canvas);
-
-    rowInput.value = " "; 
-    colInput.value = " ";
-    slider.value = 20;
-}
-
-const color = document.querySelector(".color");
-let currentColor = "#000000";
-color.addEventListener("change", ()=>{
-    currentColor = color.value;
-});
-
-const slider = document.querySelector(".slider");
-slider.addEventListener("input", ()=>{
-    const boxes = document.querySelectorAll(".box");
-    boxes.forEach((box)=>{
-        box.style.height = `${slider.value}px`;
-    })
-});
 
